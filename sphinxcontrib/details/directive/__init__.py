da350bed3876ee7da9a50e6d8c7a58a2c9a581ec
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-09-22 22:20:33 +0900

    Fix file header

--- sphinxcontrib/details/directive/__init__.py
+++ sphinxcontrib/details/directive/__init__.py
@@ -1,4 +1,10 @@
-# extension testbed
+"""
+    sphinxcontrib.details.directive
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: Copyright 2017-2019 by Takeshi KOMIYA
+    :license: Apache License 2.0, see LICENSE for details.
+"""
 
 
 from docutils import nodes
